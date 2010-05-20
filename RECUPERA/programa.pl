ae3e7fe79ffbff199c688c4b3e11efdb1bc6f0c7
Daniel de Oliveira Mantovani <daniel.oliveira.mantovani@gmail.com>
2010-05-20 00:16:04 -0300

    mais rapido

--- RECUPERA/programa.pl
+++ RECUPERA/programa.pl
@@ -3,34 +3,19 @@
 use strict;
 use warnings;
 
-my $i = 1;
-while ( my $n = <> ) {
-    chop($n);
-    my @j;
-    my $count = 0;
-    for ( split( m/ /, <> ) ) {
-        $j[$count] = $_;
-        $count++;
-    }
-    print("Instancia $i\n");
-    $i++;
-    calc( \@j, $n );
-}
-
-sub calc {
-    my ( $j, $n ) = @_;
-    my $tmp = 0;
-    if ( $n == 1 ) {
-        $j->[0] == 0 ? print("0\n\n") : print("nao achei\n\n");
-        return;
-    }
-    $tmp += $j->[0] if $j->[0] >= -30 && $j->[0] <= 30;
-    for ( my $i = 1 ; $i < $n ; ++$i ) {
-        if ( $tmp == $j->[$i] ) {
-            print("$tmp\n\n");
-            return;
+my $times = 1;
+while ( my $in = <> ) {
+    my $ak = 0;
+  AK: {
+        for ( split / /, <STDIN> ) {
+            if ( $_ == $ak ) {
+                print "Instancia $times\n$ak\n\n";
+                $times++;
+                last AK;
+            }
+            $ak += $_;
         }
-        $tmp += $j->[$i];
+        print "Instancia $times\nnao achei\n\n";
+        $times++;
     }
-    print("nao achei\n\n");
 }

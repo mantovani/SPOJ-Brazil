ea4e478a76fba154049eb08435f56b76e0222ba0
Daniel de Oliveira Mantovani <daniel.oliveira.mantovani@gmail.com>
2012-12-24 21:16:44 -0200

    faster

--- ALADES/programa.pl
+++ ALADES/programa.pl
@@ -6,6 +6,11 @@ while ( my $calc = <> ) {
     my @time = split( " ", $calc );
     exit(0) if ( $time[0] + $time[1] + $time[2] + $time[3] == 0 );
     my $count = 0;
+	if ($time[0] < $time[2]) {
+		$count = ($time[2] * 60 + $time[3]) - ($time[0] * 60 + $time[1]);
+		print "$count\n";
+		next;
+	}
 TIME: while (1) {
         while ( $time[1] <= 59 ) {
             if ( $time[0] == $time[2] && $time[1] == $time[3] ) {

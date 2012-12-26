28f3d038bb0903583584c4ce7db99b0c45d64913
Daniel de Oliveira Mantovani <daniel.oliveira.mantovani@gmail.com>
2012-12-26 15:26:30 -0200

    faster

--- ALADES/alades.hs
+++ ALADES/alades.hs
@@ -1,29 +1,13 @@
 import Control.Monad
 
 main = do
-    line <- getLine
-    let [sleepHour, sleepMin, wakeHour, wakeMin] = map read $ words line
-    let lineSum = sleepHour + sleepMin + wakeHour + wakeMin 
-    when ( lineSum /= 0 ) $ do 
-        let totalMins = minuteCount sleepHour sleepMin wakeHour wakeMin 
-        print totalMins 
-        main
-
-
-minuteCount :: (Num a, Ord a) => a -> a -> a -> a -> a
-minuteCount sHour sMin wHour wMin
-    | sHour < wHour = (wHour * 60 + wMin) - (sHour * 60 + sMin)
-    | otherwise     = minuteCountD 0 sHour sMin wHour wMin 
-
-
-minuteCountD :: (Eq a1, Eq a, Num a, Num a1, Num a2) => a2 -> a1 -> a -> a1 -> a -> a2
-minuteCountD n sHour sMin wHour wMin =
-    if sHour == wHour && sMin == wMin
-        then n
-    	else
-        if sMin == 59
-            then
-                if sHour == 23
-                    then minuteCountD (n + 1) 0 0 wHour wMin
-                	else minuteCountD (n + 1) (sHour + 1) 0 wHour wMin
-        	else minuteCountD (n + 1) sHour (sMin + 1) wHour wMin
+	line <- getLine
+	let [sHour, sMin, wHour, wMin] = map read $ words line
+	unless ( sHour == 0 && sMin == 0 && wHour == 0 && wMin == 0 ) $ do
+		let
+			sCount  =   (sHour * 60 + sMin)
+			wCount  =   (wHour * 60 + wMin)
+		if sCount < wCount
+			then print (wCount - sCount)
+			else print (1440 - (sCount - wCount))
+		main

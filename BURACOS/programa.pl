382fff4f409509938469185ca67a53a4c174b9e2
Daniel de Oliveira Mantovani <daniel.oliveira.mantovani@gmail.com>
2012-12-26 12:36:54 -0200

    working \o

--- BURACOS/programa.pl
+++ BURACOS/programa.pl
@@ -1,10 +1,35 @@
 #!/usr/bin/perl
 
-use strict;
-use warnings;
-
-my $test = 1;
-while ( my $dados = <> ) {
-    my ( $planetas, $buracos ) = split( ' ', $dados );
-    $test++;
+LOOP: while ( $dados = <> ) {
+    $count_int++;
+    ( $n, $p ) = split " ", $dados;
+    exit if $n == 0;
+    print "Teste $count_int\n";
+    @array = ();
+    for ( 1 .. $p ) {
+        ( $f, $t ) = split " ", <>;
+        $array[$f] = $t;
+    }
+    $count_ok;
+    for ( 1 .. $n ) {
+        if ( r( \@array, 1, $p, 0, $_ ) ) {
+            $count_ok++;
+        }
+        else {
+            print "N\n\n";
+            next LOOP;
+        }
+    }
+    print "S\n\n";
 }
+
+sub r {
+    ( $array, $value, $max, $count, $what ) = @_;
+    return if $count > $max;
+    if ( $array->[$value] ) {
+        return 1 if $array->[$value] == $what;
+        r( $array, $array->[$value], $max, ++$count, $what );
+    } 
+    else { return }
+}
+

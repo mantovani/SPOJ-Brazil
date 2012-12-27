d940a5885b14c96ebbb33669cb890d7b725a14d7
Daniel de Oliveira Mantovani <daniel.oliveira.mantovani@gmail.com>
2012-12-27 00:23:37 -0200

    esta correto mas ta lento

---
+++ APAGA/apaga.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define BUFFSIZE 100000
+
+main () {
+	int N,D;
+ 	int i,j,num,next;	
+	char buff[BUFFSIZE],dstr[2];
+	int nums[BUFFSIZE];
+	dstr[1] = '\0';
+	while(1) {
+		scanf("%d %d\n%s",&N,&D,buff);
+		if (N == 0 && D == 0) {
+			return 0;
+		}
+		for(i = 0;i <= (N - 1);i++) {
+			dstr[0] = buff[i];
+			nums[i] = atoi(dstr);
+		}
+
+		redo:
+		for(i = 0;i <= (N - 1);i++) {
+			if (nums[i] != -1) {
+				for(j = (i+1);j <= (N - 1);j++) {
+					if(nums[j] != -1) {
+						next = nums[j];
+						break;
+					}
+				}
+				if(next > nums[i]) {
+					nums[i] = -1;
+					D--;
+					if (D == 0) {
+						goto exgohorse;
+					} else {
+						goto redo;
+					}
+				}
+			}
+		}
+		exgohorse:
+		for(i = 0;i <= (N - 1);i++) {
+			if(nums[i] != -1) {
+				printf("%d",nums[i]);
+			}
+		}
+		printf("\n");
+	}
+}
